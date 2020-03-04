b23ceca887316119eeb68ea7fdb054aac2e5e7f9
Aleksey Belchenkov <u608110@gmail.com>
2020-03-04 15:19:35 +0300

    Initial commit

---
+++ module_pattern/app.js
@@ -0,0 +1,13 @@
+//  Basic structure
+//
+// (function () {
+//     // Declare private vars and functions
+//
+//     return {
+//         // Declare public var and functions
+//     };
+// })();
+
+const UICtrl = (function () {
+    let text = 'Hello World';
+})();
